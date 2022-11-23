f57d0395b4d85f526741764f3e341b301f70f3f2
zhoaib <zhoaibhassan@gmail.com>
2022-11-24 03:04:10 +0600

    banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import img1 from '../../../assets/images/1.png'
+import img2 from '../../../assets/images/2.png'
+import img3 from '../../../assets/images/3.png'
+
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="carousel w-full rounded-3xl">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src={img1} className="w-full" alt='' />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src={img2} className="w-full" alt='' />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src={img3} className="w-full" alt='' />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
