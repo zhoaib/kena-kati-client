747db91bfe1130fb5b48e322b3edc06a7c5cd8d8
zhoaib <zhoaibhassan@gmail.com>
2022-11-25 17:18:32 +0600

    firebase

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const { register, handleSubmit } = useForm()
+
+    const handleLogin = data => {
+        console.log(data);
+    }
+
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-3xl text-center font-bold'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input type="text" className="input input-bordered w-full max-w-xs"{...register("email")} />
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Password</span></label>
+                        <input type="password" className="input input-bordered w-full max-w-xs"{...register("password")} />
+                    </div>
+
+                    <input className='btn btn-primary w-full my-5' value='Login' type="submit" />
+                </form>
+                <p>Don't have an account? <Link className='text-primary' to='/signup'>Create new account</Link></p>
+                <div className="divider">OR</div>
+                <button className='btn btn-secondary w-full'>Login with Google</button>
+            </div>
         </div>
     );
 };
