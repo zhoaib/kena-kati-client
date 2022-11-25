2da2994bcf7c89608504c099eebfbc9ac790128e
zhoaib <zhoaibhassan@gmail.com>
2022-11-25 20:28:26 +0600

    auth

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form'
+import { AuthContext } from '../../context/AuthProvider';
 
 const SignUp = () => {
 
-    const { register, handleSubmit } = useForm()
-
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const { createUser } = useContext(AuthContext)
     const handleSignUp = (data) => {
         console.log(data)
+        createUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.log(error));
     }
 
     return (
@@ -18,17 +25,26 @@ const SignUp = () => {
 
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Name</span></label>
-                        <input type="text" {...register("name")} className="input input-bordered w-full max-w-xs" />
+                        <input type="text" {...register("name", {
+                            required: "Name is required"
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
                     </div>
 
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Email</span></label>
-                        <input type="email" {...register("email")} className="input input-bordered w-full max-w-xs" />
+                        <input type="email" {...register("email", {
+                            required: true
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
                     </div>
 
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Password</span></label>
-                        <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs" />
+                        <input type="password" {...register("password", {
+                            required: "Password is required"
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
                     </div>
 
                     <input className='btn btn-primary w-full my-5' value='Sign Up' type="submit" />
