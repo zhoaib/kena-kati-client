f98d76209ad51dfe9264e6b02f6656134888dc9e
zhoaib <zhoaibhassan@gmail.com>
2022-11-26 03:21:05 +0600

    loading-setloading

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -3,8 +3,12 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthProvider';
 
 const PrivateRoute = ({ children }) => {
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
+
+    if (loading) {
+        return <progress className="progress w-56"></progress>
+    }
 
     if (user) {
         return children;
--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -8,13 +8,16 @@ const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
 
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
     const signIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
@@ -23,6 +26,7 @@ const AuthProvider = ({ children }) => {
     }
 
     const logOut = () => {
+        setLoading(true);
         return signOut(auth);
     }
 
@@ -30,6 +34,7 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             console.log('user observing');
             setUser(currentUser);
+            setLoading(false);
         });
         return () => unsubscribe();
     }, [])
@@ -40,6 +45,7 @@ const AuthProvider = ({ children }) => {
         logOut,
         updateUser,
         user,
+        loading,
     }
 
     return (
