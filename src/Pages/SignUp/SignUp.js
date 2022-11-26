a58138a9e186ae20d4c3dda401f8005ea7530d4d
zhoaib <zhoaibhassan@gmail.com>
2022-11-26 21:21:40 +0600

    token

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -44,8 +44,19 @@ const SignUp = () => {
         })
             .then(res => res.json())
             .then(data => {
-                console.log('save user', data);
-                navigate('/');
+                getUserToken(email);
+
+            })
+    }
+
+    const getUserToken = email => {
+        fetch(`http://localhost:5000/jwt?email=${email}`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.accessToken) {
+                    localStorage.setItem('accessToken', data.accessToken);
+                    navigate('/');
+                }
             })
     }
 
